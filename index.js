6c6e1831ecfa6bdbb4f693722034ed1b1cd081cb
arunkumarcognitiveclouds.com <47918140+bossmonkey98@users.noreply.github.com>
2023-05-28 16:11:57 +0530

    Player movements iimplemented

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ canvas.width = 1260;
 canvas.height = 800;
 
 c.fillRect(0,0,canvas.width,canvas.height)
-let gravity = 0.2
+let gravity = 0.7
 class Sprite {
     constructor({position , velocity}){
     this.position = position;
@@ -18,7 +18,10 @@ draw(){
 }
 update(){
     this.draw()
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y 
+    this.lastKey
+    
     if(this.position.y + this.height + this.velocity.y >=canvas.height){
         this.velocity.y = 0
     }
@@ -45,12 +48,73 @@ velocity:{
 }})
 
 
+let keys = {
+    a:{pressed:false},
+    d:{pressed:false},
+    ArrowLeft:{pressed:false},
+    ArrowRight:{pressed:false},
+}
+
 const animate = ()=>{
     window.requestAnimationFrame(animate)
     c.fillStyle = "#000"
     c.fillRect(0,0,canvas.width,canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x =0
+    if(keys.a.pressed && player.lastKey==="a") player.velocity.x = -5;
+    else if(keys.d.pressed && player.lastKey==="d") player.velocity.x =5;
+
+    enemy.velocity.x = 0
+    if(keys.ArrowLeft.pressed && enemy.lastKey==="ArrowLeft") enemy.velocity.x = -5;
+    else if(keys.ArrowRight.pressed && enemy.lastKey==="ArrowRight") enemy.velocity.x =5;
+    
 }
 
 animate()
+
+
+
+window.addEventListener('keydown',(e)=>{
+switch(e.key){
+    case "d":
+        keys.d.pressed=true
+        player.lastKey="d"
+        break;
+    case "a":
+        keys.a.pressed = true
+        player.lastKey="a"
+        break;
+    case "w":
+        player.velocity.y = -20
+        break;
+    case "ArrowLeft":
+        keys.ArrowLeft.pressed = true
+        enemy.lastKey = "ArrowLeft"
+        break;
+    case "ArrowRight":
+        keys.ArrowRight.pressed = true
+        enemy.lastKey = "ArrowRight"
+        break;
+    case "ArrowUp":
+        enemy.velocity.y = -20
+}
+})
+
+window.addEventListener('keyup',(e)=>{
+    switch(e.key){
+        case "d":
+            keys.d.pressed=false
+            break
+        case "a":
+            keys.a.pressed=false
+            break;
+        case "ArrowLeft":
+            keys.ArrowLeft.pressed = false
+        case "ArrowRight":
+            keys.ArrowRight.pressed = false
+
+    }
+})
+
